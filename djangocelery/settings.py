cb3e17aaeb74229b35be3d29e05f8cf9a9a35b65
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-11-20 16:15:34 +0800

    Fix celery timezone value.

--- djangocelery/settings.py
+++ djangocelery/settings.py
@@ -131,7 +131,7 @@ CELERY_RESULT_BACKEND = "redis://redis:6379/0"  # or your result backend
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "UTC"  # or your timezone
+CELERY_TIMEZONE = "Australia/Perth"
 
 # For django-celery-beat
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
